6aa2f8280527692932c005d710873558fa4a0816
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-07-26 23:32:08 +0900

    로직 완성 및 README 작성

--- MMdeploy.js
+++ MMdeploy.js
@@ -2,29 +2,24 @@ const axios = require('axios');
 
 // 웹훅을 통해 메시지 전송
 async function sendWebhook(message) {
-  const webhookURL = process.env.MM_WEBHOOK_URL; // GitHub Secrets에 저장된 웹훅 URL을 사용
+  // const webhookURL = process.env.MM_WEBHOOK_URL; // GitHub Secrets에 저장된 웹훅 URL을 사용
+  const webhookURL =
+    'https://meeting.ssafy.com/hooks/m1t17fm6tintuniyio3yxhyd8y';
 
   const body = {
     text: message,
   };
 
   try {
-    const response = axios.post(webhookURL, body, {
+    const response = await axios.post(webhookURL, body, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    console.log(response);
+    console.log(response.data);
   } catch (error) {
     console.error('Error :', error);
   }
 }
 
-// 배포 로직
-async function deploy() {
-  const message = `# 승호야 안녕!`;
-  await sendWebhook(message);
-}
-
-// 배포 스크립트 실행
-deploy();
+module.exports = { sendWebhook };
